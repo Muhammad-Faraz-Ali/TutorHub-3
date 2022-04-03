b0231fbb0cdc6998688d1262b38fbdf39453838f
Muhammad-Faraz-Ali <farazalipucit@gmail.com>
2022-04-03 21:20:43 +0500

    Faraz is commiting

--- src/navigation/notificationPages/index.js
+++ src/navigation/notificationPages/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from '../../screens/home/index.js'
+import HomeScreen from '../../screens/home/index.js';
 import SearchScreen from '../../screens/search/index.js';
-import NotificationScreen from '../../screens/notifications/index.js' 
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import NotificationScreen from '../../screens/notifications/index.js';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 const TopTab = createMaterialTopTabNavigator();
 
 function index({navigation}) {
   return (
-    <TopTab.Navigator screenOptions={({ route }) => ({
+    <TopTab.Navigator
+      screenOptions={({route}) => ({
         // tabBarIcon: ({ focused, color, size }) => {
         //   let iconName;
 
@@ -28,20 +29,20 @@ function index({navigation}) {
         //   if(focused)
         //       size=25
         //   else
-        //       size=15    
+        //       size=15
         //   return <Ionicons name={iconName} size={size} color={color} />;
         // },
         tabBarActiveTintColor: 'maroon',
         tabBarInactiveTintColor: 'black',
-        headerShown:false,
-        tabBarLabelStyle:{elevation:10,fontSize:18},
-      })} initialRouteName='Teacher'>
-      <TopTab.Screen name="Teacher" component={NotificationScreen}/>
-      <TopTab.Screen name="Student" component={NotificationScreen}/>
-      <TopTab.Screen name="Other" component={NotificationScreen}/>
+        headerShown: false,
+        tabBarLabelStyle: {elevation: 10, fontSize: 18},
+      })}
+      initialRouteName="Teacher">
+      <TopTab.Screen name="Teacher" component={NotificationScreen} />
+      <TopTab.Screen name="Student" component={NotificationScreen} />
+      <TopTab.Screen name="Other" component={NotificationScreen} />
     </TopTab.Navigator>
   );
-
 }
 
 export default index;
